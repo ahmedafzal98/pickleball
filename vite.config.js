f610a08152d4247999e2bd89ad7e1ea195edaee3
Ahmed Afzal <ahmed.afzal2070@gmail.com>
2025-02-08 03:22:18 +0500

    deployemnt

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // Ensure relative path for Netlify
   plugins: [react()],
+  build: {
+    assetsDir: '', // Ensure correct asset serving
+  },
 })
